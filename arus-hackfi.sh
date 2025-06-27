8264e28a46c4df621c93d79f81e8285d1b555e90
Ariyan Islam Munna <aruiyaaa03@gmail.com>
2025-06-28 00:27:38 +0600

    arus-hackfi.sh
    # Aru's Hack Fi - WiFi Hacking Toolkit 🛡️
    
    **Developed by AriYan Munna (T71C)**  
    WiFi Ethical Hacking Tool built for Termux + Kali NetHunter users to scan, capture, and test WiFi security.
    
    ## 🔥 Features:
    - Monitor Mode Activation
    - Airodump-ng based WiFi Scanning
    - Handshake Capture
    - Dictionary-based Password Cracking
    - WPS Pixie Dust Attack
    - Adapter & Mode Checker
    
    ## ⚙️ Installation:
    ```bash
    pkg update && pkg install git -y
    git clone https://github.com/aruiyaaa03/Aru-HackFi.git
    cd Aru-HackFi
    chmod +x arus-hackfi.sh
    bash arus-hackfi.sh

---
+++ arus-hackfi.sh
@@ -0,0 +1,20 @@
+# Aru's Hack Fi - WiFi Hacking Toolkit 🛡️
+
+**Developed by AriYan Munna (T71C)**  
+WiFi Ethical Hacking Tool built for Termux + Kali NetHunter users to scan, capture, and test WiFi security.
+
+## 🔥 Features:
+- Monitor Mode Activation
+- Airodump-ng based WiFi Scanning
+- Handshake Capture
+- Dictionary-based Password Cracking
+- WPS Pixie Dust Attack
+- Adapter & Mode Checker
+
+## ⚙️ Installation:
+```bash
+pkg update && pkg install git -y
+git clone https://github.com/aruiyaaa03/Aru-HackFi.git
+cd Aru-HackFi
+chmod +x arus-hackfi.sh
+bash arus-hackfi.sh
